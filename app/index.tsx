06ca476671c53623503de6c8dc1796f4e6c1e0fc
matheusCsousa <matheus.thelas@hotmail.com>
2026-02-15 16:38:00 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+
+interface Pokemon {
+  name: string;
+  url: string;
+}
+
+export default function Index() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  async function fetchPokemons() {
+    try {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=20",
+      );
+      const data = await response.json();
+      setPokemons(data.results);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return (
+    <ScrollView>
+      {pokemons.map((pokemon) => (
+        <View key={pokemon.name}>
+          <Text>{pokemon.name}</Text>
+        </View>
+      ))}
+    </ScrollView>
+  );
+}
